1a730368d208d5db2d526deb21f07622c1b92f5b
Robert <robert@qubitdigital.com>
2015-03-27 22:13:48 +0000

    Meow

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# event-kitten
-An event emitter inspired by event-kit, written in JavaScript
+Event Kitten
+------------
+
+An event emitter inspired by [event-kit](https://github.com/atom/event-kit), written in JavaScript. Meow.
+
+### Motivation
+
+[event-kit](https://github.com/atom/event-kit) is pretty awesome however it is written in coffeescript :disappointed: and uses [Grim](https://github.com/atom/grim) which is a >1MB dependency!
+
+
+### API
+
+```javascript
+var createEmitter = require('event-kitten')
+var emitter = createEmitter()
+
+var sub = emitter.on('foo', function () {
+  console.log('bar')
+})
+
+// logs 'bar'
+emitter.emit('foo')
+
+// remove the observer
+sub.dispose()
+```
---
+++ test/test-event-kitten.js
@@ -0,0 +1,52 @@
+/* global describe, it, beforeEach, expect */
+
+var sinon = require('sinon')
+var createEmitter = require('../event-kitten')
+
+describe('emitter', function () {
+  var emitter = createEmitter()
+  beforeEach(function () {
+    emitter = createEmitter()
+  })
+  it('should not throw errors when no observers are present', function () {
+    expect(emitter.on).withArgs('foo').to.not.throwException()
+  })
+  it('should invoke the handler only when the named event is emitted', function () {
+    var handler = sinon.stub()
+    emitter.on('foo', handler)
+    expect(handler.callCount).to.be(0)
+
+    emitter.emit('bar')
+    expect(handler.callCount).to.be(0)
+
+    emitter.emit('foo')
+    expect(handler.callCount).to.be(1)
+  })
+  it('should invoke more than one handler observing the same named event', function () {
+    var handlerA = sinon.stub()
+    var handlerB = sinon.stub()
+    emitter.on('foo', handlerA)
+    emitter.on('foo', handlerB)
+
+    emitter.emit('foo')
+    expect(handlerA.callCount).to.be(1)
+    expect(handlerB.callCount).to.be(1)
+  })
+  it('should not invoke the observer when the dispose method is called', function () {
+    var handlerA = sinon.stub()
+    var handlerB = sinon.stub()
+    var subscription = emitter.on('foo', handlerA)
+    emitter.on('foo', handlerB)
+
+    emitter.emit('foo')
+    subscription.dispose()
+    emitter.emit('foo')
+    expect(handlerA.callCount).to.be(1)
+    expect(handlerB.callCount).to.be(2)
+  })
+  it('should not throw errors if dispose is called twice', function () {
+    var subscription = emitter.on('foo', sinon.stub())
+    subscription.dispose()
+    expect(subscription.dispose).to.not.throwException()
+  })
+})
